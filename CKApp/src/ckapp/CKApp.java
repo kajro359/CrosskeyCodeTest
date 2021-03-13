fd1c6df1cde367688d44dcb667c570922aad1e04
Kaj Rolandsson <kajrolandsson@gmail.com>
2021-03-13 08:50:44 +0100

    clarence line is clean now

--- CKApp/src/ckapp/CKApp.java
+++ CKApp/src/ckapp/CKApp.java
@@ -11,7 +11,7 @@ package ckapp;
 //import java.util.Scanner;
 import java.io.File;
 import java.util.Scanner;
-//import org.me.CKLib.CKLib;
+import org.me.CKLib.CKLib;
 //import org.me.CKLib.MyMaths;
 import org.me.CKLib.Prospect;
 
@@ -24,23 +24,47 @@ public class CKApp {
 //    private static String cleanString(String s) {
 //        
 //    }
-     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        Prospect p = new Prospect("Jessica", 30, 300000, 5, 1);        
-        p.print();          
-        System.out.println(Prospect.COUNT);
-        String path = "../Data/prospects.txt";
-        String line = "";
-        Scanner sc = new Scanner(new File(path));
-        sc.useDelimiter("\n");
-        while (sc.hasNext()) {
-            line = sc.next();
-            
-            System.out.println(line);
-        }
+        // Prospect p = new Prospect("Jessica", 30, 300000, 5, 1);
+        // p.print();
+       
+        String line = "\"Clarencé,Andersson\",2000,6,4";
+        System.out.println(line);
+        String cleanLine = CKLib.cleanLine(line);
         
+        
+        
+//        String path = "../Data/prospects.txt";
+//        String line = "";
+//        Scanner sc = new Scanner(new File(path));
+//        sc.useDelimiter("\n");
+//        
+//        while (sc.hasNext()) {
+//            line = sc.next();
+//
+//            if(!line.matches("^[A-Za-zÅÄÖåäöÉéÜüß]*.[A-Za-zÅÄÖ*åäöÉéÜüß]*,"
+//                    + "[0-9]*.[0-9]*,[0-9]*.[0-9]*,[0-9]*")) {
+//                System.out.println("Cleaning line...");
+//                line = CKLib.cleanLine(line);
+//                //System.out.println(line);        
+//            }             
+//       
+//
+//            System.out.println(line);
+            // split the line into name, loan, interest, years
+            // instanciate prospect
+
+            //System.out.println(a);
+//            String[] data = line.split("[,]");
+//            
+//            for(String w: data) {
+//                System.out.print(w+ " ");
+//            }
+//            System.out.println("\n");
+//        }
+
     }
 }
--- CKLib/src/org/me/CKLib/CKLib.java
+++ CKLib/src/org/me/CKLib/CKLib.java
@@ -5,8 +5,8 @@
  */
 package org.me.CKLib;
 
-import java.io.File;
-import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -14,5 +14,47 @@ import java.util.Scanner;
  */
 public class CKLib {
 
+    private static char[] extractChars(String s, int ind0, int ind1) {
+        char[] ret = new char[ind1];
+        s.getChars(ind0, ind1, ret, 0);
+        return ret;
+    }
+
+    public static String cleanLine(String line) {
+        // String a = line.replaceAll("[^A-Za-zÅÄÖåäöé0-9,]", "?");        
+        String a = "";
+        Pattern p = Pattern.compile("[A-Za-zÅÄÖåäöÉéÜüß]+."
+                + "[A-Za-zÅÄÖåäöÉéÜüß]*");
+
+        Matcher m = p.matcher(line);
+
+        if (m.find()) {
+            int ind0 = m.start();
+            int ind1 = m.end();
+            System.out.println("INDICES: " + ind0 + ", " + ind1);
+
+            String name = "";
+            char[] nm = extractChars(line, ind0, ind1);
+            for (char c : nm) {
+                System.out.print(c);
+                name += c;
+            }
+            name = name.replaceAll("[^A-Za-zÅÄÖåäöÉéÜüß]", " ");
+            name = name.strip();
+            System.out.println("Made name-string: \n" + name);
+            a += name;
+            // get numbers
+            char[] nums = extractChars(line, ind1 + 1, line.length());
+            for (char c : nums) {
+                System.out.print(c);
+                a += c;
+            }
+
+        }
+
+        System.out.println("\n" + a);
+
+        return a;
+    }
 
 }
